742ca2fe6e5781d8a13179f64a110860360fe5c0
keylarindani <keylarindani08@gmail.com>
2025-03-24 17:40:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import joblib
 
-st.title('🎈 App Name')
+def load_model(filename):
+  model = joblib.load(filename)
+  return model
 
-st.write('Hello world!')
+def predict_with_model(model, user_input):
+  prediction = model.predict([user_input])
+  return prediction[0]
+
+def main():
+  st.title('Dermatology Machine Learning')
+  st.info('This app using machine learning')
